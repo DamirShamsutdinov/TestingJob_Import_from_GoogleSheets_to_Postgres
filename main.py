59c0a48532e37b5702103a4c961c427675484b8e
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-08-07 04:13:31 +0300

    more readable

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 import os.path
 from config import host, user, password, db_name
-from curs import currency_dict
-
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
 from psycopg2 import connect
 from psycopg2.extras import execute_values
 
+from curs import currency_dict
+
+'''Parser Google Sheets API'''
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials.json')
 SAMPLE_SPREADSHEET_ID = '1g-dwgL6aJABx64hp7LWubff_VhSjVzCDnsl92yhedLU'
 SAMPLE_RANGE_NAME = 'List1!A:D'
 
-
-def parser_sheets(sc, s_a_f, s_s_i, s_r_n):
-    '''Parser Google Sheets API'''
-    credentials = service_account.Credentials.from_service_account_file(
-        s_a_f, scopes=sc)
-    service = build('sheets', 'v4', credentials=credentials)
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=s_s_i,
-                                range=s_r_n).execute()
-    values = result.get('values', [])
-    response = service.spreadsheets().values().get(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID,
-        majorDimension='ROWS',
-        range=SAMPLE_RANGE_NAME
-    ).execute()
-    data = response['values'][1:]
-    return data
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+service = build('sheets', 'v4', credentials=credentials)
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                            range=SAMPLE_RANGE_NAME).execute()
+values = result.get('values', [])
+response = service.spreadsheets().values().get(
+    spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    majorDimension='ROWS',
+    range=SAMPLE_RANGE_NAME
+).execute()
+data = response['values'][1:]
 
 
 def push_postgres(values):
@@ -48,11 +45,11 @@ def push_postgres(values):
 
     cr.execute(
         """
-        create table test_c(
+        create table if not exists test_c(
                 transaction_id int primary key ,
                 order_id int,
                 amount_usd numeric,
-                amount_rub numeric generated always as (amount_usd * %s) stored,
+                amount_rub numeric generated always as (amount_usd * %s) stored ,
                 deadlines_delivery date
         );
         """,
@@ -77,5 +74,6 @@ def push_postgres(values):
     cr.close()
     c.close()
 
+
 if __name__ == '__main__':
-    push_postgres(parser_sheets(SCOPES , SERVICE_ACCOUNT_FILE, SAMPLE_SPREADSHEET_ID, SAMPLE_RANGE_NAME))
+    push_postgres(data)
